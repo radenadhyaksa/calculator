a74bef84ea9d925e239dc43d6eec38dd351b400b
radenadhyaksa <raksa69@gmail.com>
2022-03-30 12:55:12 +0700

    update main.dart - add class CalculatorButton

--- lib/main.dart
+++ lib/main.dart
@@ -49,3 +49,30 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
+
+class CalculatorButton extends StatelessWidget {
+  final Color backgroundColor;
+  final Color foregroundColor;
+  final String text;
+  CalculatorButton({
+    required this.backgroundColor,
+    required this.foregroundColor,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: backgroundColor,
+      child: Center(
+        child: Text(
+          text,
+          style: Theme.of(context)
+              .textTheme
+              .headline4!
+              .copyWith(color: foregroundColor),
+        ),
+      ),
+    );
+  }
+}
